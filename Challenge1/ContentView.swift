92f72dbccecb762f156fc6142e2261b96624a3b3
corry.handayani <corzz_93@yahoo.com>
2021-08-05 22:35:27 +1000

    - Option A to load Firestore data using foreach

--- Challenge1/ContentView.swift
+++ Challenge1/ContentView.swift
@@ -14,24 +14,46 @@ struct ContentView: View {
     var body: some View {
         //initialise class inside state object
         
-        // may have to change to ScrollView
+    //Initially trying this to load firestore db
+     // ForEach(accountVM.account, id: \.id) {
+     //   account in
+      //    Text(account.acctype)}
+        
         VStack {
-            VStack{
-                
-                //testing to see if the data load correctly
-                ForEach(accountVM.account, id: \.id) {
-                    account in
-                    Text(account.acctype)
-                }
             
-                
+            ForEach(accountVM.account, id: \.id) {
+                account in
+           
+            VStack{
+                //testing to see if the data load correctly
                //Top card
                 VStack {
                     
                     TopSection()
              
                     //one customer may have many accounts
-                    AccountCard()
+                    // account card
+                    VStack(alignment: .leading){
+                        HStack{
+                            Text(account.acctype)
+                                .fontWeight(.regular)
+                            Spacer()
+                            Text(account.balance)
+                                .fontWeight(.semibold)
+                        }
+                        .padding(.bottom,4)
+                        .modifier(Header2())
+                        //.frame(maxWidth: .infinity, alignment: .leading)
+                        // added alignment on Vstack - code efficiency
+                        
+                        Text("Card ending in ...\(account.cardnum)")
+                            .modifier(TextGray())
+                        
+                    }
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 3)
+                            .stroke(Color("LightGray")))
                        
                     
                 }
@@ -47,8 +69,12 @@ struct ContentView: View {
             .overlay(
             RoundedRectangle(cornerRadius:3)
                 .stroke(Color("LightGray"), lineWidth: 1))            }
+            
+            //end Vstack
+            
+        }
         .padding()
-     
+       
     }
 }
 
